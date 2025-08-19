501bbbc03761a05eacbc7833e502272e0e59e9c7
gretzke <daniel@gretzke.de>
2025-08-19 04:32:23 +0200

    Add git credential helper

--- shared/api/src/v1/plugins/repo-manager/types.ts
+++ shared/api/src/v1/plugins/repo-manager/types.ts
@@ -3,6 +3,17 @@
 
 import { PathOptions } from "@ignite/plugin-types/base/repo-manager";
 
+// Git credentials interface for secure credential passing
+export interface GitCredentialsData {
+  initialized: boolean;
+  credentialTypes: string[];
+  ssh?: {
+    hasAgent: boolean;
+    hasKeyPath: boolean;
+    keyPath?: string;
+  };
+}
+
 export interface CheckoutBranchRequest extends PathOptions {
   branch: string;
 }
